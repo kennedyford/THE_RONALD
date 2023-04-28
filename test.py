b4542ba354b9459b7487aac472049f28afa53dac
kennedyford <knf010@email.latech.edu>
2023-04-28 15:03:47 -0500

    second test to make sure the ans can change

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-def testy():
-    return "This is the output!"
+def testy(num):
+    if num == 1:
+        return "This is the output!"
+    elif num == 2:
+        return "This is a different output!"
+    else:
+        return "how'd you do that bro?"
