04d69bf6d210086df2b12014e5b782317205022d
kennedyford <knf010@email.latech.edu>
2023-04-29 10:01:58 -0500

    moved test file to folders

---
+++ DoS/test.py
@@ -0,0 +1,7 @@
+def testy(num):
+    if num == 1:
+        return "This is the output!"
+    elif num == 2:
+        return "This is a different output!"
+    else:
+        return "how'd you do that bro?"
