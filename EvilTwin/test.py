86312a07774e60a6bd3a3d2be26f66cba871f32f
kennedyford <knf010@email.latech.edu>
2023-04-29 13:29:48 -0500

    changed file structure

--- EvilTwin/test.py
+++
@@ -1,7 +0,0 @@
-def testy(num):
-    if num == 1:
-        return "This is the output!"
-    elif num == 2:
-        return "This is a different output!"
-    else:
-        return "how'd you do that bro?"
